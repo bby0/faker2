32eb2227ed5bf22d47b26ce33963ee80a53ae074
bby0 <nuanbaobao123@Gmail.com>
2021-07-18 03:16:57 +0800

    Update jd_cashHelp.py

--- jd_cashHelp.py
+++ jd_cashHelp.py
@@ -13,7 +13,7 @@ update 2021.7.17 15:02
 #ck 优先读取【JDCookies.txt】 文件内的ck  再到 ENV的 变量 JD_COOKIE='ck1&ck2' 最后才到脚本内 cookies=ck
 cookies = ''
 # 设置被助力的账号可填用户名 或 pin的值不要; env 设置 export cash_zlzh="用户1&用户N"
-cash_zlzh = ['Your JD_User', '买买买']
+cash_zlzh = ['jd_58c7396bf1d5c', '为什么还是抢不到啊']
 
 # Env环境设置 通知服务
 # export BARK=''                   # bark服务,苹果商店自行搜索;
